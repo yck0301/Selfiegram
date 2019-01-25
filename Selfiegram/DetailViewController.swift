eb7831441b0ba1b74b50d0909b3fc96355a08399
Yu-Chiang Kuo <yu-chiangkuo@yu-chiangde-mac-mini.local>
2019-01-25 16:25:29 +0800

    Initial Commit

---
+++ Selfiegram/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Selfiegram
+//
+//  Created by Yu-Chiang Kuo on 2019/1/25.
+//  Copyright © 2019 Yu-Chiang Kuo. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
